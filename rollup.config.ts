ebbef2a749b7457bf2919708517698e5c27d1733
Jesse Jafa <jesse.jafa@gmail.com>
2022-01-18 13:02:09 +0200

    initial split to cjs & es6

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,6 +1,5 @@
 import postcss from 'rollup-plugin-postcss';
 import typescript from 'rollup-plugin-typescript2';
-import pkg from './package.json';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 export default {
