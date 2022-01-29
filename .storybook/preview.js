b83309068ff0bf89e86630038b050f3b371a21f6
Kostas Siabanis <ksiabani@users.noreply.github.com>
2022-01-29 15:21:15 +0200

    Add Tabs and introduce CSS variables for theming
    * Add @mui/base to use unstyled components
    * Add Tabs component from MUI's unstyled tabs
    * Add stories for Tabs
    * Add global CSS variables for easier theming
    * Minor fix in Table
    * Bump version to 0.10.8
    
    closes Add tabs #65

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
-import { StylesProvider } from '@mui/styles';
 import StyledEngineProvider from '@mui/material/StyledEngineProvider';
 import { createMatteTheme } from '../src/components/utilities/createMatteTheme.component';
+import { setGlobalStyles } from '../src/components/utilities/setGlobalStyles';
 import '!style-loader!css-loader!sass-loader!./stories.scss';
 
 const theme = createMatteTheme();
@@ -32,6 +32,7 @@ export const decorators = [
   (Story) => (
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={theme}>
+        {setGlobalStyles(theme)}
         <Story />
       </ThemeProvider>
     </StyledEngineProvider>
