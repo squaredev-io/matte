b41870b77ec4b663cd183a51289ff844fad13b32
Ana Colell <68306689+anacolell@users.noreply.github.com>
2022-02-24 11:20:05 +0200

    Adds multiple select to select component (#93)
    * Adds multiple select to select component
    * Bumps version

--- src/components/inputs/select/select.stories.tsx
+++ src/components/inputs/select/select.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Select } from './select.component';
+import { Box, SelectChangeEvent } from '@mui/material';
+import Chip from '@mui/material/Chip';
 
 export default {
   title: 'Components/Inputs/Select',
@@ -44,6 +46,77 @@ export const Selects: ComponentStory<typeof Select> = () => {
         label="Required"
         items={selectItems}
         required
+      />
+    </div>
+  );
+};
+
+export const MultipleSelect: ComponentStory<typeof Select> = () => {
+  const selectItems = [
+    { value: 1, text: '1' },
+    { value: '2', text: '2' },
+    { value: 3, text: '3' },
+  ];
+
+  const [items, setItems] = React.useState<string[]>([]);
+
+  const handleChange = (event: SelectChangeEvent<typeof items>) => {
+    const {
+      target: { value },
+    } = event;
+    setItems(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value
+    );
+  };
+
+  return (
+    <div className="story__form-field">
+      <Select
+        id="select-multiple"
+        items={selectItems}
+        multiple
+        value={items}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export const MultipleSelectChip: ComponentStory<typeof Select> = () => {
+  const selectItems = [
+    { value: 1, text: '1' },
+    { value: '2', text: '2' },
+    { value: 3, text: '3' },
+  ];
+
+  const [items, setItems] = React.useState<string[]>([]);
+
+  const handleChange = (event: SelectChangeEvent<typeof items>) => {
+    const {
+      target: { value },
+    } = event;
+    setItems(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value
+    );
+  };
+
+  return (
+    <div className="story__form-field">
+      <Select
+        id="select-multiple"
+        items={selectItems}
+        multiple
+        value={items}
+        onChange={handleChange}
+        renderValue={(selected: any) => (
+          <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+            {selected.map((value: any) => (
+              <Chip key={value} label={value} />
+            ))}
+          </Box>
+        )}
       />
     </div>
   );
